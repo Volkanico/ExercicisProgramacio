8f3782e4588ed1f42fbf08435f5b57899fd56609
Volkanico <vcekmez15181@alumnes.iesmanacor.cat>
2021-01-19 11:59:13 +0100

    Actualitzat

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,37 @@
 package com.company;
 
+import java.io.*;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Scanner sc = new Scanner(System.in);
+        int num;
+        boolean exit = false;
+
+        System.out.println("Introdueix un text nou: ");
+        System.out.println("Llegeix el text escrit: ");
+        System.out.println("Compta el numero de lletres: ");
+        System.out.println("Compta el numero de paraules: ");
+        System.out.println("Numero d'aparicions per caracter: ");
+        System.out.println("El caracter mes repetit i el numero d’aparicions: ");
+        System.out.println("La paraula mes repetida i el numero d'aparicions: ");
+        System.out.println("Introduir per teclat un caracter i dir quantes vegades apareix: ");
+        System.out.println("Introduir per teclat una paraula i dir quantes vegades apareix: ");
+
+        while(!exit){
+            num = sc.nextInt();
+            if(num == 1) {
+                exit = true;
+
+            }
+
+
+        }
+
+        Practica1 p1 = new Practica1();
+        p1.PracticaEx2();
     }
 }
--- src/com/company/Practica1.java
+++ src/com/company/Practica1.java
@@ -1,2 +1,74 @@
-package com.company;public class Practica1 {
+package com.company;
+import java.util.Scanner;
+
+public class Practica1 {
+
+    private String paraula;
+
+    public void setParaula(String paraula) {
+        this.paraula = paraula;
+    }
+
+    public String getParaula(){
+        return paraula;
+    }
+
+    public String PracticaEx1 (String text) {
+
+        Scanner reader = new Scanner(System.in);
+
+        System.out.println("Introdueix el text:");
+
+        paraula = reader.nextLine();
+
+        return paraula;
+    }
+    public void PracticaEx2 () {
+        Scanner reader = new Scanner(System.in);
+
+        System.out.println(getParaula());
+    }
+
+    public void PracticaEx3 () {
+        int counter = 0;
+
+        for(int i = 0; i < paraula.length(); i++) {
+
+            if (paraula.charAt(i) != ' ' && paraula.charAt(i) != '.' && paraula.charAt(i) != ',' &&
+                    paraula.charAt(i) != '?' && paraula.charAt(i) != '!' && paraula.charAt(i) != ':') {
+                counter ++;
+            }
+
+        }
+        System.out.println("El numero lletres registrat es: " + counter);
+    }
+    public void PracticaEx4 () {
+        int counter = 0;
+
+        for(int i = 0; i < paraula.length(); i++) {
+            if (paraula.charAt(i) != ' ' && paraula.charAt(i) != '.' && paraula.charAt(i) != ',' &&
+                    paraula.charAt(i) != '?' && paraula.charAt(i) != '!' && paraula.charAt(i) != ':') {
+                else if(paraula.(i) + 1)
+
+
+            }
+        }
+        System.out.println("El numero paraules registrades es: " + counter);
+    }
+
+    public void PracticaEx5 () {
+        int counter = 0;
+        int numeroC = 0;
+        for(int i = 0; i < paraula.length(); i++) {
+        }
+        switch (numeroC) {
+            case 1:
+                paraula.
+
+
+
+        }
+    }
+
 }
+
